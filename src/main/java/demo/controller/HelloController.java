1404cb92803389ce086e82826929efa367f72cfb
AlexCiocan <alex.ciocan.lx@gmail.com>
2014-12-04 14:37:58 +0200

    Working em integration

--- src/main/java/demo/controller/HelloController.java
+++ src/main/java/demo/controller/HelloController.java
@@ -1,16 +1,29 @@
 package demo.controller;
 
+import org.apache.log4j.spi.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ch.qos.logback.classic.Logger;
+import demo.domain.LeadData;
+import demo.repository.LeadStorageRepository;
+
 
 @Controller
 public class HelloController {
+	
+	
+	
+	@Autowired
+	private LeadStorageRepository leadStorageRepository;
+	
     @RequestMapping("/hello")
     public ModelAndView index() {
         ModelAndView mav=new ModelAndView("hello");
     	mav.addObject("helloMessage", "Greetings from GradleSpringBoot");
+    	LeadData data=leadStorageRepository.getLeadData(1l);
         return mav;
     }
 }
